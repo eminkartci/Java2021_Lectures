4265d7b7f025b149b0840ff93588e02c54fe98b1
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-12 22:30:05 +0300

    Triangular Right method

--- BasicsProgram/App.java
+++ BasicsProgram/App.java
@@ -4,7 +4,8 @@ public class App{
 
     public static void main(String[] args) {
         
-        Shape.draw_triangular();
+        Shape.draw_left_triangular(7,'x');
+        Shape.draw_right_triangular(3,'w');
 
     }
 
--- BasicsProgram/Shape.java
+++ BasicsProgram/Shape.java
@@ -4,38 +4,49 @@ public class Shape {
 
 
 
-    public static void draw_triangular(){
-
-        /*
-
-        h = 7
-
-        o
-        oo
-        ooo
-        oooo
-        ooooo
-        oooooo
-        ooooooo
-
-        */
+    public static void draw_left_triangular(int h,char c){
 
         // 1. row 1
         // 2. row 2
         // 3. row 3
 
         // All Rows
-        for(int rowIndex = 0 ; rowIndex < 7 ; rowIndex++) {
+        for(int rowIndex = 0 ; rowIndex < h ; rowIndex++) {
 
             // ROW
             for (int charCount = 0 ; charCount <= rowIndex ; charCount++){
-                System.out.print("o");
+                System.out.print(c);
             }
 
             System.out.println();
         }
 
 
+
+    }
+
+    public static void draw_right_triangular(int h,char c){
+
+        // 1.row 2  h-1 
+        // 2.row 1  h-1 -1
+        // 3.row 0  h-1 -2
+
+
+        // All Rows
+        for(int rowIndex = 0 ; rowIndex < h ; rowIndex++) {
+
+            // ROW
+                // SPACE
+                for(int spaceCount = 0 ; spaceCount < (h-1)-rowIndex ; spaceCount ++){
+                    System.out.print(" ");
+                }
+                // CHAR
+                for (int charCount = 0 ; charCount <= rowIndex ; charCount++){                
+                    System.out.print(c);
+                }
+
+            System.out.println();
+        }
 
     }
     
