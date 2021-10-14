6660d3e0a5597f977dc1bb0d12e163c89fe8c4ac
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-14 22:19:14 +0300

    create Shape method is implemented

--- BasicsProgram/App.java
+++ BasicsProgram/App.java
@@ -1,26 +1,16 @@
 package BasicsProgram;
 
 public class App{
-
     public static void main(String[] args) {
-        
-        Shape tri = new Shape("Triangular");
-        Shape rect = new Shape("Rectangular");
-        Shape dim = new Shape("Diamond",20,'I');
 
         Shape userShape = createShape("Diamond");
-
-        // System.out.println(tri);
-        // System.out.println(rect);
-        // System.out.println(dim);
-
         System.out.println(userShape);
     }
 
     public static Shape createShape(String userShapeStr){
         
-        Shape tri = new Shape(userShapeStr);
-        return tri;
+        Shape userShape = new Shape(userShapeStr);
+        return userShape;
     }
 
 
