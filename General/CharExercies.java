0672f27d64b491f4a3fe3f4df4d173f90a414d2f
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-19 22:19:51 +0300

    Char exercise

---
+++ General/CharExercies.java
@@ -0,0 +1,16 @@
+public class CharExercies {
+
+    public static void main(String[] args) {
+        
+        char randomChar = 'e';
+        String randomStr = "e";
+
+        System.out.println((char) (randomChar + 13));
+        System.out.println(randomStr + 13);
+
+    }
+
+    
+
+    
+}
