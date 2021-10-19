a845be4c372d5a04ee56bd40decc571042a96879
eminkartci <emin.kartci@ozu.edu.tr>
2021-10-19 22:33:35 +0300

    For Each example

---
+++ General/ForEachExercise.java
@@ -0,0 +1,35 @@
+public class ForEachExercise {
+
+    public static void main(String[] args) {
+
+        // numbers array
+        int[] numbers = {23,34,435,2324,636,77,252,1245,634,63,2,178}; // 
+
+        // iterate 0 to 5
+        // for(int index = 0 ; index < 5 ; index++){
+
+        //     System.out.println(index);
+
+        // }
+
+        // iterate numbers
+        // for(int i = 0 ; i < numbers.length; i++){
+
+        //     System.out.println(numbers[i]);
+
+        // }
+
+        // FOR EACH
+        for(int x : numbers){
+            System.out.print(x + " ");
+        }
+
+
+
+
+    }
+
+
+
+    
+}
